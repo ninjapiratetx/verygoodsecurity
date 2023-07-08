cf1fbc116357ea9dd36f7d216ad8020fb915efb3
ninjapiratetx <ninjapiratetx@gmail.com>
2023-07-08 15:32:20 -0500

    moving out a variable to it's correct spot

--- src/static_values.py
+++ src/static_values.py
@@ -1,8 +1,17 @@
 from tkinter import W
 
+from test.test_get_post import test_get_post_by_id
+
 
 HTTP_SUCCESS = 200
 HTTP_SUCCESS_POST = 201
 URI = "https://jsonplaceholder.typicode.com/"
 API_CALL = "posts"
+POST_URI = f"{URI}{API_CALL}"
 ID = "id"
+NAME = "name"
+BODY = "body"
+USERID = "userId"
+TEST_USERID = 1
+TEST_NAME = "test"
+TEST_BODY = "test test"
